928a047ee377e37ebac0459075a6151a0a335d43
Mattia Riboni <154544827+mat-riboni@users.noreply.github.com>
2024-09-02 11:24:29 +0200

    First commit

---
+++ src/test/java/it/uniroma3/federazione/SiwFederazioneApplicationTests.java
@@ -0,0 +1,13 @@
+package it.uniroma3.federazione;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SiwFederazioneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
