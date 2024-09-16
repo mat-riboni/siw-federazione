d7176c32b23687fc828a8e7ae670ea71dff2d580
Mattia Riboni <154544827+mat-riboni@users.noreply.github.com>
2024-09-16 17:20:17 +0200

    main modificato

--- src/main/java/it/uniroma3/federazione/SiwFederazioneApplication.java
+++ src/main/java/it/uniroma3/federazione/SiwFederazioneApplication.java
@@ -18,7 +18,7 @@ import it.uniroma3.federazione.service.CredentialsService;
 import it.uniroma3.federazione.service.SquadraService;
 
 @SpringBootApplication
-public class SiwFederazioneApplication implements CommandLineRunner {
+public class SiwFederazioneApplication{
 
 	@Autowired
 	SquadraService squadraService;
@@ -29,56 +29,4 @@ public class SiwFederazioneApplication implements CommandLineRunner {
 	public static void main(String[] args) {
 		SpringApplication.run(SiwFederazioneApplication.class, args);
 	}
-	
-	@Override
-	public void run(String... args) throws Exception {
-		for(int i = 0; i < 10; i++) {
-			Squadra squadra = new Squadra();
-			squadra.setName("Squadra "+i);
-			squadra.setAnno(2000+i);
-			squadra.setSede("Roma");
-			List<Giocatore> giocatori = new ArrayList<>();
-			for(int j = 0; j < 9; j++) {
-				Giocatore giocatore = new Giocatore();
-				giocatore.setNome("Sergio");
-				giocatore.setCognome(Integer.toString(j));
-				giocatore.setDataNascita(LocalDate.now());
-				giocatore.setLuogoNascita("Roma");
-				giocatore.setNumeroMaglia(j);
-				giocatore.setRuolo("Ala piccola");
-				giocatore.setSquadra(squadra);
-				if(j <= 4) {
-					giocatore.setTitolare(true);
-				} else {
-					giocatore.setTitolare(false);
-				}
-				giocatori.add(giocatore);
-			}
-			squadra.setGiocatori(giocatori);
-			Presidente presidente = new Presidente();
-			presidente.setNome("Sergio");
-			presidente.setCognome("Allevi");
-			presidente.setCodiceFiscale("LLVSRG72E19H501W");
-			presidente.setDataNascita(LocalDate.of(1970, 4, 12));
-			presidente.setLuogoNascita("Genova");
-			presidente.setSquadra(squadra);
-			squadra.setPresidente(presidente);
-			Credentials cred = new Credentials();
-			cred.setUsername("pres" + i);
-			cred.setPassword(Integer.toString(i));
-			cred.setPresidente(presidente);
-			cred.setRole(Credentials.PRESIDENTE_ROLE);
-			squadraService.save(squadra);
-			credentialsService.saveCredentials(cred);
-
-		}
-		Credentials admin = new Credentials();
-		admin.setUsername("adminProva");
-		admin.setPassword("12");
-		admin.setRole(Credentials.ADMIN_ROLE);
-		credentialsService.saveCredentials(admin);
-	}
-	
-	
-
 }
